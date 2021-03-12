af1c735b5d153e35e4d33532a06dadf8a7796c40
pHoran17 <c16416742@mydit.ie>
2021-03-12 18:10:58 +0000

    Updated predict screen to recieve output and graph for current iteration of the linear regressions model. Flask backend has code added to facilitate this

--- F1ML/src/screens/PredictScreen.js
+++ F1ML/src/screens/PredictScreen.js
@@ -23,13 +23,15 @@ export default class PredictScreen extends React.Component
 
 	async componentDidMount()
 	{
-			const url1 = 'http://192.168.0.17:5000/';
+			const url1 = 'http://192.168.0.17:5000/model';
 			const url2 = 'http://192.168.0.17:5000/graph';
 			const req1 = axios.get(url1);
 			const req2 = axios.get(url2);
 
 
-			/*axios.all([req1, req2]).then(axios.spread((...responses) => {
+			/*
+			//Causes Unhandled promis rejection when splitting responses, use individual calls instead
+			axios.all([req1, req2]).then(axios.spread((...responses) => {
 				const res1 = responses[0]
 				const res2 = responses[1];
 				const b64Image = res2.data.ImageBytes;
@@ -37,22 +39,35 @@ export default class PredictScreen extends React.Component
 				console.log(decodeImage);
 				this.setState ({
 
-					//graphData:res2.data,
+					modelData: res1.data.data,
+					graphData:res2.data.ImageBytes,
 					isloading: false
 				});
 				
 			}).catch(errors => {
 				console.error(errors);
-			}))*/
+			}))
+			*/
+			axios.get(url1).then(res => {
+				//console.log(res.data.data);
+				const modelReq = res.data.data; 
+				//const decodeImage = base64.decode(b64Image);
+				this.setState({
+					modelData:res.data.data
+				});
+			});
+
 
 			axios.get(url2).then(res => {
 				//console.log(res);
 				const b64Image = res.data.ImageBytes;
+				//console.log(this.state.modelData);
 				//const decodeImage = base64.decode(b64Image);
 				this.setState({
 					graphData:b64Image,
 					isloading:false
 				});
+				//console.log(this.state.modelData);
 			});
 
 			
@@ -62,12 +77,27 @@ export default class PredictScreen extends React.Component
 	{
 		if(this.state.graphData.length !== 0){
 			
-			console.log(typeof(this.state.graphData));
+			//console.log(this.state.modelData);
 			return(
 				<>
 					<Header />
+					<FlatList 
+							data={this.state.modelData}
+							keyExtractor={(item,index) => item.index}
+							renderItem={({item, index}) => (
+								<ListItem>
+									<ListItem.Content>
+										<Text>
+											Intercept (MS): {item.intercepts}
+										</Text>
+										<Text>
+											Coefficent (MS): {item.coefficents}
+										</Text>
+									</ListItem.Content>
+								</ListItem>
+							)}
+						/>
 					<View style={styles.container}>
-						<Text>Model Data Here</Text>
 						<Image style={styles.graph} source={{uri: `data:image/png;base64,${this.state.graphData}`}}/>
 					</View>
 					<View style={styles.buttons}>
@@ -78,7 +108,7 @@ export default class PredictScreen extends React.Component
 		}
 		else
 		{
-			console.log(this.state.graphData);
+			//console.log(this.state.graphData);
 			return(
 				<>
 					<Header />
