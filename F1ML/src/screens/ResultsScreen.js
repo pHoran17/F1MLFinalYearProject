f76fb4ab3145d1dd2da2889e7e47630464e49dc3
pHoran17 <c16416742@mydit.ie>
2021-04-02 00:18:11 +0100

    Added functionalty for choosing results for a particular race on Results screen

--- F1ML/src/screens/ResultsScreen.js
+++ F1ML/src/screens/ResultsScreen.js
@@ -40,13 +40,34 @@ export default class ResultsScreen extends React.Component
 	async componentDidMount()
 	{
 			const url1 = 'http://192.168.0.17:5000/res';
-			//const url2 = 'http://192.168.0.17:5000/graph';
-			//const req1 = axios.get(url1);
+			const url2 = 'http://192.168.0.17:5000/raceList';
+            const pRequest = {
+                data:{
+                    race: this.state.race
+                },
+                headers:{
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                responseType: 'json',
+                url: url1
+            };
+            const listRequest = {
+                headers:{
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                responseType: 'json',
+                url: url2
+            };
+			//const req1 = axios.post(url1, pRequest);
 			//const req2 = axios.get(url2);
             const race = this.state.race;
             //const refer = await Firebase.database().ref('results');
             const items = new Array();
-            const pickerItems = new Array();
+            //const pickerItems = new Array();
             //const racePair = new Object();
             /*refer.on('value',(function (snapshot) 
             {
@@ -196,43 +217,29 @@ export default class ResultsScreen extends React.Component
                 console.log(res);
             });*/
             //Firebase.database().ref('/results/data').child().
-			/*
+			
 			//Causes Unhandled promis rejection when splitting responses, use individual calls instead
-			axios.all([req1, req2]).then(axios.spread((...responses) => {
+			/*axios.all([req1, req2]).then(axios.spread((...responses) => {
 				const res1 = responses[0]
 				const res2 = responses[1];
-				const b64Image = res2.data.ImageBytes;
-				const decodeImage = base64.decode(b64Image);
-				console.log(decodeImage);
-				this.setState ({
-
-					modelData: res1.data.data,
-					graphData:res2.data.ImageBytes,
-					isloading: false
-				});
+                const rList = res2.data;
+                this.setInitResults(res1.data.data,rList);
 				
 			}).catch(errors => {
 				console.error(errors);
-			}))
-			*/
-            const pRequest = {
-                data:{
-                    race: this.state.race
-                },
-                headers:{
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                },
-                responseType: 'json',
-                url: url1
-            };
+			}))*/
+			
+            
+            /*axios.get(url2).then(response => {
+                //pickerItems = response.data;
+                //console.log(rList);
+                console.log(response.data);
+            }).catch(function(error){
+                console.log(error);
+            });*/
             axios.post(url1, pRequest).then(response => {
                 //console.log(response.data);
-                /*this.setState = ({resultData: response.data}, () => {
-                    console.log(this.state.resultData);
-                });*/
-                this.changeResults(response.data.data);
+                this.setInitResults(response.data.data);
                 //console.log(this.state.resultData);
             }).catch(function(error){
                 console.log(error);
@@ -261,19 +268,87 @@ export default class ResultsScreen extends React.Component
 
 			
 	}
-    async changeResults(data){
-        await this.setState({resultData: data});
-        console.log(this.state.resultData);
+    /*async getPickerList()
+    {
+        const url2 = 'http://192.168.0.17:5000/raceList';
+        axios.get(url2).then(response => {
+            console.log(response.data);
+            const pickItems = response.data;
+            return pickItems;
+            //console.log(rList);
+        }).catch(function(error){
+            console.log(error);
+        });
+    }*/
+    async setInitResults(data){
+        //const races = [];
+        const url2 = 'http://192.168.0.17:5000/raceList';
+        await axios.get(url2).then(response => {
+            //console.log(response.data);
+            const races = response.data.data;
+            this.setState({resultData: data, pickerList: races});
+            //console.log(rList);
+        }).catch(function(error){
+            console.log(error);
+        });
+        //console.log(races);
+        
+        //console.log(this.state.pickerList);
+    }
+    async setResults(data){
+        const raceRequest = {
+            data:{
+                race: data
+            },
+            headers:{
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            responseType: 'json',
+            url: url1
+        };
+        axios.post(url1, raceRequest).then(response => {
+           //await this.setState({resultData: response.data.data, race:data});
+           return response.data.data
+            
+        }).catch(function(error){
+            console.log(error);
+        });
     }
     async changeRace(data){
-        await this.setState({race: data});
+        //const newResults = setResults(data);
+        //console.log(this.state.race);
+        //await this.setState({race:data});
         console.log(this.state.race);
+        const crUrl = 'http://192.168.0.17:5000/res';
+        const raceRequest = {
+            data:{
+                race: data
+            },
+            headers:{
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            responseType: 'json',
+            url: crUrl
+        };
+        await axios.post(crUrl, raceRequest).then(response => {
+           //await this.setState({resultData: response.data.data, race:data});
+            this.setState({race: data, resultData: response.data.data});
+            
+        }).catch(function(error){
+            console.log(error);
+        });
+        
     }
 	render()
 	{
 		if(this.state.resultData.length !== 0){
 			
-			console.log(this.state.resultData);
+			//console.log(this.state.pickerList);
+            console.log(this.state.race);
 			return(
 				<>
 					<View style={styles.header}>
@@ -281,28 +356,9 @@ export default class ResultsScreen extends React.Component
 					</View>
                     <View style={styles.dropdownContainer}>
                         <DropDownPicker
-                            items={[
-                                {label: 'Abu Dhabi 2020', value:'Abu Dhabi Grand Prix 2020'},
-                                {label: 'Sakhir 2020', value:'Sakhir Grand Prix 2020'},
-                                {label: 'Bahrain 2020', value:'Bahrain Grand Prix 2020'},
-                                {label: 'Turkey 2020', value:'Turkish Grand Prix 2020'},
-                                {label: 'Imola 2020', value:'Emilia Romagna Grand Prix 2020'},
-                                {label: 'Portugal 2020', value:'Portuguese Grand Prix 2020'},
-                                {label: 'Germany 2020', value:'Eifel Grand Prix 2020'},
-                                {label: 'Russian 2020', value:'Russian Grand Prix 2020'},
-                                {label: 'Mugello 2020', value:'Tuscan Grand Prix 2020'},
-                                {label: 'Monza 2020', value:'Italian Grand Prix 2020'},
-                                {label: 'Belgium 2020', value:'Belgian Grand Prix 2020'},
-                                {label: 'Spain 2020', value:'Spanish Grand Prix 2020'},
-                                {label: '70th Anniversary Grand Prix 2020', value:'70th Anniversary Grand Prix 2020'},
-                                {label: 'Great Britain 2020', value:'British Grand Prix 2020'},
-                                {label: 'Hungary 2020', value:'Hungarian Grand Prix 2020'},
-                                {label: 'Styria 2020', value:'Styrian Grand Prix 2020'},
-                                {label: 'Austria 2020', value:'Austrian Grand Prix 2020'},
-
-                        ]}
+                            items={this.state.pickerList}
                             style={styles.dropdown}
-                            onChangeItem={item => this.changeRace(item)}
+                            onChangeItem={item => this.changeRace(item.value)}
                         />
                     </View>
 					<View style={styles.resultsList}>
@@ -314,7 +370,7 @@ export default class ResultsScreen extends React.Component
 									<ListItem.Content>
                                     <ListItem.Title style={styles.resultTitle}>{item.positionText}</ListItem.Title>
 												<Text style={styles.driver}>{item.driver}</Text>
-												
+												<Text style={styles.constructor}>{item.constructor}</Text>
 												<Text style={styles.points}>{item.points}</Text>
 												<Text style={styles.time}>{item.time_x}</Text>
 									</ListItem.Content>
